0f228daf0057cf98d0faea1a0bfb152717ce4ed3
Anna-JuMacedo <annajuliamacedo16@gmail.com>
2025-06-12 11:06:45 -0300

    css

--- formulario.php
+++ formulario.php
@@ -9,6 +9,7 @@ include 'conexão.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width-device-width, initial-scale-1.0">
     <title>Formulário de Cadastro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h2>Cadastro do Usuário</h2>
